051bdb6ffb64d2e3d9c5edf9f9fca09cc5b14e05
algosketch <sjy_9484@naver.com>
2021-10-17 23:48:38 +0900

    add domain & memory repository

---
+++ src/main/java/basic/basicspring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package basic.basicspring.repository;
+
+import basic.basicspring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}

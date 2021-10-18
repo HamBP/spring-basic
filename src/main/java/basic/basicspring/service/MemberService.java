aa1ce94e1d0003aa9aaeb19a388755eff412d308
HamBP <sjy_9484@naver.com>
2021-10-18 15:31:14 +0900

    add member service

---
+++ src/main/java/basic/basicspring/service/MemberService.java
@@ -0,0 +1,40 @@
+package basic.basicspring.service;
+
+import basic.basicspring.domain.Member;
+import basic.basicspring.repository.MemberRepository;
+import basic.basicspring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 동명이인 금지
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
